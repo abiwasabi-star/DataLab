f08f75c0546ac24376844cf7558bda0af3bea7cc
abigaelwasabi <94919325+abiwasabi-star@users.noreply.github.com>
2026-01-18 17:10:18 +0300

    Add square_sum function to calculate square of sum

--- k.py
+++ k.py
@@ -5,3 +5,13 @@ def sum(a, b): #arguments
 c = sum(90, 12) #params
 print(c)
 c = 102
+
+def square_sum():
+    x = float(input("Enter value for x: "))
+    y = float(input("Enter value for y: "))
+    
+    result = (x + y) * (x + y)
+    print("Result:", result)
+
+# calling the function
+square_sum()
